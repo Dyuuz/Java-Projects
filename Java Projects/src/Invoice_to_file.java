cd5127aa5982522137203d261da84bfa4fabac1a
Dyuuz <lekanoyesunle@gmail.com>
2023-07-17 03:19:28 +0100

    Invoice to file

--- Java Projects/src/Invoice_to_file.java
+++ Java Projects/src/Invoice_to_file.java
@@ -1,6 +1,22 @@
+import javax.swing.JOptionPane;
+import java.awt.Component;
+import java.io.FileWriter;
 
 public class Invoice_to_file {
+	
+	private static final Component frame = null;
 	public static void main(String []args) {
+//		Welcome info to Invoice-File users 
+		String wlcmsg = "Welcome to Invoice to File project";
+		Object [] wlc = {"Lauch Project"};
+		JOptionPane.showOptionDialog(frame, ""+wlcmsg, "Invoice-File Project", JOptionPane.YES_OPTION,
+				JOptionPane.PLAIN_MESSAGE, null, wlc, wlc[0]);
+		String appinfo = "This project helps to create an invoice and convert to different file format";
+		Object [] aim = {"Proceed"};
+		JOptionPane.showOptionDialog(frame, ""+appinfo, "Invoice-File Project", JOptionPane.YES_OPTION,
+				JOptionPane.PLAIN_MESSAGE, null, aim, aim[0]);
+		String invoice_name = JOptionPane.showInputDialog("Input the Invoice project name");
+		JOptionPane.showMessageDialog(null, "Your Invoice name is "+invoice_name);
 		
 	}
 
