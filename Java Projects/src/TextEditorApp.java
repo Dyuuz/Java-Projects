91f932b010672542b7b6f8a83a121ce66113ee8f
Dyuuz <lekanoyesunle@gmail.com>
2023-08-18 04:20:46 +0100

    Modified

--- Java Projects/src/TextEditorApp.java
+++ Java Projects/src/TextEditorApp.java
@@ -1,9 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
 
 public class TextEditorApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame Frame = new JFrame();
+		JMenuBar Menu = new JMenuBar();
+		JMenuItem Menu = new JMenuItem();
+		JTextArea Text = new JTextArea();
+		
 	}
 
 }
