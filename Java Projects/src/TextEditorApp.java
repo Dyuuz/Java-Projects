79b61ca816b31eff908f813e1f70183f60f10649
Dyuuz <lekanoyesunle@gmail.com>
2023-08-18 04:19:29 +0100

    New projects

---
+++ Java Projects/src/TextEditorApp.java
@@ -0,0 +1,9 @@
+
+public class TextEditorApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java Projects/src/TicTacToe.java
@@ -0,0 +1,12 @@
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		int a = 50;
+		double b = 50.4;
+		boolean c = (a == b);
+		System.out.print(c);
+
+	}
+
+}
